b134f5faba8232d6df507fcb33dfd7970092f7b5
Andrew Krawchyk <akrawchyk@optoro.com>
2017-08-04 15:55:01 -0400

    fix test by mocking not friday date

--- amweekly/tests/test_management.py
+++ amweekly/tests/test_management.py
@@ -17,5 +17,7 @@ def test_postlinks_command_friday(share, mocker):
 @pytest.mark.django_db
 def test_postlinks_command_not_friday(share, mocker):
     mocked = mocker.patch('amweekly.slack.jobs.process_incoming_webhook.delay')
+    mocked_dt = mocker.patch('amweekly.management.commands.postlinks.datetime.datetime')  # noqa
+    mocked_dt.utcnow.return_value = datetime(2017, 6, 22)  # NOT FRIDAY
     call_command('postlinks')
     assert not mocked.called
