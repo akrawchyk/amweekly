d7ba5616446ff8de83bf45cfc695c6cca47af332
Jeremy Carbaugh <jeremy@jcarbaugh.com>
2018-03-06 16:23:46 -0500

    freezegun instead of mocking datetime

--- amweekly/tests/test_management.py
+++ amweekly/tests/test_management.py
@@ -1,6 +1,5 @@
-from datetime import datetime
-
 from django.core.management import call_command
+from freezegun import freeze_time
 
 import pytest
 
@@ -8,16 +7,14 @@ import pytest
 @pytest.mark.django_db
 def test_postlinks_command_friday(share, mocker):
     mocked = mocker.patch('amweekly.slack.jobs.process_incoming_webhook.delay')
-    mocked_dt = mocker.patch('amweekly.management.commands.postlinks.datetime.datetime')  # noqa
-    mocked_dt.utcnow.return_value = datetime(2017, 6, 23)  # FRIDAY
-    call_command('postlinks')
+    with freeze_time("2017-06-23"):
+        call_command('postlinks')
     assert mocked.called
 
 
 @pytest.mark.django_db
 def test_postlinks_command_not_friday(share, mocker):
     mocked = mocker.patch('amweekly.slack.jobs.process_incoming_webhook.delay')
-    mocked_dt = mocker.patch('amweekly.management.commands.postlinks.datetime.datetime')  # noqa
-    mocked_dt.utcnow.return_value = datetime(2017, 6, 22)  # NOT FRIDAY
-    call_command('postlinks')
+    with freeze_time("2017-06-22"):
+        call_command('postlinks')
     assert not mocked.called
